57d44704dba47421e4a5b4b1d3f29937a31f1f83
DoJapHD <contact@dojaphd.com>
2023-03-22 17:47:38 +0100

    Update Gradle plugin
    Signed-off-by: DoJapHD <contact@dojaphd.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ labyMod {
                 "1.18.2",
                 "1.19.2",
                 "1.19.3",
-                "1.19.4-rc1",
+                "1.19.4",
         ) { version, provider ->
             configureRun(provider, version)
         }
